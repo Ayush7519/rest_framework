6e3daa6c7a89fd4b801712dc53b3c1f705654d8b
aayush <ayushkandel7519@gmail.com>
2023-03-21 13:57:23 +0545

    url changes

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('api/create',views.hostcreateapiview.as_view())
+    path('api/create/',views.hostcreateapiview.as_view())
 ]
